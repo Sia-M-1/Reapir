6f5822f814b3ebe2cf56d587bb85abbebfbf9d45
Sia-M-1 <sia.razumovskaya@mail.ru>
2026-05-12 08:50:29 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+import db
+from utils import write_msg
+
+# Функционал администратора: просмотр заявок, изменение статуса.
+# Для заглушки:
+def handle_admin(user_id, message):
+    write_msg(user_id, "Админ-панель пока не реализована.")
---
+++ registration.py
@@ -0,0 +1,7 @@
+import db
+from utils import write_msg
+
+# Здесь будет логика регистрации: запрос ФИО, должности, роли, пароля и т. д.
+# Для заглушки:
+def handle_registration(user_id, message):
+    write_msg(user_id, "Регистрация пока не реализована.")
