4b47a45d33cfde47fd734a90968f9ebc05acec4c
Sia-M-1 <sia.razumovskaya@mail.ru>
2026-05-14 13:42:09 +0300

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,7 +1,1 @@
-import db
-from utils import write_msg
 
-# Здесь будет логика регистрации: запрос ФИО, должности, роли, пароля и т. д.
-# Для заглушки:
-def handle_registration(user_id, message):
-    write_msg(user_id, "Регистрация пока не реализована.")
