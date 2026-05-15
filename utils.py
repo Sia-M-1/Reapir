233b68d3afdefd9cfcefd9efc879363c972bf9ab
Sia-M-1 <sia.razumovskaya@mail.ru>
2026-05-15 09:41:19 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,9 @@
 import vk_api
 import json
-
-# Импортируем объект vk из config.py, чтобы иметь доступ к API
 from config import vk
 
 def write_msg(user_id, message, keyboard=None):
-    """
-    Универсальная функция для отправки сообщения пользователю.
-    Принимает ID пользователя, текст и (опционально) клавиатуру.
-    """
+    """Универсальная функция для отправки сообщения пользователю. Принимает ID пользователя, текст и клавиатуру"""
     params = {'user_id': user_id, 'message': message, 'random_id': 0}
     if keyboard:
         import json
